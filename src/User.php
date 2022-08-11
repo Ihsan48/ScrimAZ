5d9a0b49487a2976da9d3c0a4cd0d704779a15e7
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-08-11 13:32:20 +0700

    Formating

--- src/User.php
+++ src/User.php
@@ -3,33 +3,32 @@
 namespace OnlyJaiden\ScrimAS;
 
 use OnlyJaiden\ScrimAS\Main;
+
 use pocketmine\utils\Config;
 use pocketmine\player\Player;
 
+class User {
 
-class User{
     public $User = [];
     
-    public function checkAlert(Player $player) : void{
+    public function checkAlert(Player $player) : void {
         $config = new Config('plugin_data/ScrimAS/'."user.yml", Config::YAML);
         $new = Main::getInstance()->getConfig();
-        if($config->get($player->getName()) == true) {
+        if ($config->get($player->getName()) == true) {
             $config->set($player->getName(), false);
             $player->SendMessage($new->get("AntiCheat.prefix")." Your alerts are now ENABLED!");
-        } elseif($config->get($player->getName()) == false) {
+        } elseif ($config->get($player->getName()) == false) {
             $config->set($player->getName(), true);
             $player->SendMessage($new->get("AntiCheat.prefix")." Your alerts are now DISABLED!");
         }
         $config->save();
-        
     }
     
-    public function getUser(Player $staff, string $cheat, string $player) : void{
-         $config = new Config('plugin_data/ScrimAS/'."user.yml", Config::YAML);
-         $new = Main::getInstance()->getConfig();
-         if($config->get($staff->getName()) == false) {
-             $staff->SendMessage($new->get("AntiCheat.prefix")." $player has been using $cheat.");
-         }
+    public function getUser(Player $staff, string $cheat, string $player) : void {
+        $config = new Config('plugin_data/ScrimAS/'."user.yml", Config::YAML);
+        $new = Main::getInstance()->getConfig();
+        if ($config->get($staff->getName()) == false) {
+            $staff->sendMessage($new->get("AntiCheat.prefix")." $player has been using $cheat.");
+        }
     }
-
 }
